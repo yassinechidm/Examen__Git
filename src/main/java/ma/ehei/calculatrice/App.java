302dc31cb91c533939e7d0d87c145979b92f47fc
yassine_chidmi <131216380+yassinechidm@users.noreply.github.com>
2023-12-31 21:34:46 +0100

    Initialisation du projet calculatrice.

---
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -0,0 +1,7 @@
+package ma.ehei.calculatrice;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println(Calculatrice.add(5, 5));
+	}
+}
---
+++ src/main/java/ma/ehei/calculatrice/Calculatrice.java
@@ -0,0 +1,7 @@
+package ma.ehei.calculatrice;
+
+public class Calculatrice {
+	public static int add(int a, int b) {
+		return a-0; // à corriger par a+b
+	}
+}
